b38275e5521070ecc1a4f361e23a66af553d2da8
李占昆 <lizhankun@ymt360.com>
2018-07-30 20:05:26 +0800

    DesignPatterns init

---
+++ DesignPatterns/DesignPatterns/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DesignPatterns
+//
+//  Created by 李占昆 on 2018/7/30.
+//  Copyright © 2018年 kkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DesignPatterns/DesignPatterns/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignPatterns
+//
+//  Created by 李占昆 on 2018/7/30.
+//  Copyright © 2018年 kkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
